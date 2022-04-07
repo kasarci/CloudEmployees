e04f73af8529e573e1242561e2227940afda340d
kasarci <muhammedkasarci@hotmail.com>
2022-04-07 15:29:34 +0200

    - Created CloudEmployeeDataContext class. - Configured primary keys, columns and relationships.

--- CloudEmployees.Entities/Concrete/Address.cs
+++ CloudEmployees.Entities/Concrete/Address.cs
@@ -7,6 +7,7 @@ public class Address : IEntity {
   public string Street { get; set; }
   public string City { get; set; }
   public string Zipcode { get; set; }
+  public string Country { get; set; }
   public Company? Company { get; set; }
   public Employee? Employee { get; set; }
 }
